9507c5e253f981f38c09afd3b140b38982bcbd37
Thimithi Weerathunga <20201964@student.curtin.edu.au>
2020-09-09 22:20:00 +0530

    Insert function

--- back-end/server.js
+++ back-end/server.js
@@ -1,15 +1,99 @@
 const express = require("express");
+const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
 
+//app.use(express.static('public'));
+app.use(cors());
+app.use(express.json());
+//app.use(express.bodyParser.urlencoded({ extended: true }));
+//app.use(express.bodyParser.json());
+
 app.get("/events",(req,res) => {
     const events = [
-        {id : 1, fName : 'Anuka', lName : 'Jayasundara'},
-        {id : 2, fName : 'Maneesha', lName : 'Rajapaksha'},
-        {id : 3, fName : 'Prabhasha', lName : 'Amarathunga'}
-    ];
-    res.json(events);
+         {eventId : 1, eventName : 'How to Invest in Share Market', hostingAffiliation : 'IEEE Young Professionals Sri Lanka',date : "March 27, 2018" ,time : "5:30 pm to 8:30 pm", status : "Open"},
+         {eventId : 2, eventName : 'IEEE Sri Lanka Section AGM 2018', hostingAffiliation : 'IEEE Sri Lanka Section',date : "Feb 7, 2018" ,time : "5:30 pm to 10:00 pm", status : "Open"},
+       {eventId : 3, eventName : 'Cloud Study Jam 2018', hostingAffiliation : 'GDG Cloud Sri Lanka',date : "January 13, 2018" ,time : "9:00 am to 3:30 pm", status : "Closed"}
+   ];
+   res.json(events);
+     //console.log(req.body);
+ });
+
+
+app.post("/editAssignedMem",cors(), (req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    //res.send("Hi");
+    console.log("Edit Assign Mem");
+    console.log(req.body);
+    res.json({stat: 'good'});
 });
+
+app.post("/editDesignation", cors(), (req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    //res.send("Hi");
+    console.log("Edit Designation");
+    console.log(req.body);
+    res.json({ stat: 'good' });
+});
+
+app.post("/createDesignation", cors(), (req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    //res.send("Hi");
+    console.log("Create Designation");
+    console.log(req.body);
+    res.json({ stat: 'good' });
+});
+
+app.post("/addAffiliation", cors(), (req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    //res.send("Hi");
+    console.log("Add Affiliation");
+    console.log(req.body);
+    res.json({ stat: 'good' });
+});
+
+app.get("/affiliation",(req,res) => {
+    const affiliation = [
+        {affiliationId : 1, affiID : '001', affiliationtype : 'Student Branch', affiliationname:"UOC" ,  affiliationno: "IEEE001"  , date : "March 27, 2018" , status : "Available"},
+        {affiliationId : 2, affiID : '002', affiliationtype : 'Women In Engineering',affiliationname:"SLIIT" ,affiliationno: "IEEE002"  , date : "Feb 7, 2018" , status : "Available"},
+        {affiliationId : 3, affiID : '003', affiliationtype : 'Young Professionals',affiliationname:"UOP" ,affiliationno: "IEEE003"  , date : "January 13, 2018" , status : "Not Available"}
+    ];
+    res.json(affiliation);
+    //console.log(req.body);
+});
+
+app.get("/brochure",(req,res) => {
+    const brochure = [
+        {bid: 1 , title: 'Gold', msg: '24k Bracelet' },
+        {bid: 2 ,title: 'Silver', msg: 'Necklace' }
+
+    ];
+    res.json(brochure);
+    //console.log(req.body);
+});
+
+
+
+app.get("/EventView/:id", (req, res) => {
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type");
+    console.log(req.params.id);
+    const event = [
+        {eventId : 3, eventName : 'Cloud Study Jam 2018', hostingAffiliation : 'GDG Cloud Sri Lanka',date : "January 13, 2018" ,time : "9:00 am to 3:30 pm", status : "Closed"}
+    ];
+    res.json(event);
+});
+
+app.get("/manageaffiliation/:id", (req, res) => {
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type");
+    console.log(req.params.id);
+    const manageaffiliation = [
+        {affiliationId : 1, affiID : '001', affiliationtype : 'Student Branch', affiliationname:"UOC" ,  affiliationno: "IEEE001"  , date : "March 27, 2018" , status : "Available"},
+        {affiliationId : 2, affiID : '002', affiliationtype : 'Women In Engineering',affiliationname:"SLIIT" ,affiliationno: "IEEE002"  , date : "Feb 7, 2018" , status : "Available"},
+        {affiliationId : 3, affiID : '003', affiliationtype : 'Young Professionals',affiliationname:"UOP" ,affiliationno: "IEEE003"  , date : "January 13, 2018" , status : "Not Available"}
+    ];
+    res.json(event);
+});
+
 
 app.get("/contact",(req,res) => {res.send("Contact me at anuka@GMAIL.COM");});
 
--- front-end/src/admin/App.jsx
+++ front-end/src/admin/App.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
 import NavBar from './components/Dashboard/NavBar';
+import OldSideBar from './components/Dashboard/OldSideBar';
 import SideBar from './components/Dashboard/SideBar';
 import ContentHeader from './components/Dashboard/ContentHeader';
 import Footer from './components/Dashboard/Footer';
 import EventTable from './components/Event-management/EventTable';
-import StudentBranchTable from './components/StudentBranch-management/StudentBranchTable';
-import StudentBranchForm from './components/StudentBranch-management/StudentBranchForm';
-import ManageStudentBranch from './components/StudentBranch-management/ManageStudentBranch';
 import EventView from './components/Event-management/EventView';
 import EventForm from './components/Event-management/EventForm';
 import MemberRequest from './components/Member-management/MemberRequest';
@@ -20,84 +18,29 @@ import ActivityTable from './components/Activity-log/ActivityTable';
 import EventReportTable from './components/EventReport-management/EventReportTable';
 import EventReportForm from './components/EventReport-management/EventReportForm';
 import EventAttendance from './components/EventAttendance-management/EventAttendance';
+import AffiliationTable from './components/StudentBranch-management/AffiliationTable';
+import StudentForm from './components/StudentBranch-management/StudentForm'
+import ManageAffiliation from './components/StudentBranch-management/ManageAffiliation'
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 function App() {
-
-    const [page, setPage] = useState("Event");
-    const [display, setDisplay] = useState({ event: "none", memberreq: "none", eventView: "none", eventForm: "none", memberadd: "none", designation: "none", activity: "none", createDesignationForm: "none", designationChair: "none", editAssigned: "none", editDesignation: "none" , studentbranch : "none" , studentbranchForm : "none", managestudentbranch : "none",eventR : "none", eventRAdd : "none", eventRApp : "none", eventAtt : "none"});
-
-    function showContent(pageName) {
-        setPage(pageName);
-        switch (pageName) {
-            case "Event": setDisplay({ event: "", memberreq: "none", eventView: "none", eventForm: "none", memberadd: "none", designation: "none", activity: "none", createDesignationForm: "none", designationChair: "none", editAssigned: "none", editDesignation: "none" , studentbranch : "none" , studentbranchForm : "none", managestudentbranch : "none",eventR : "none", eventRAdd : "none", eventRApp : "none", eventAtt : "none"});
-                break;
-            case "Member Request": setDisplay({ event: "none", memberreq: "", eventView: "none", eventForm: "none", memberadd: "none", designation: "none", activity: "none", createDesignationForm: "none", designationChair: "none", editAssigned: "none", editDesignation: "none" , studentbranch : "none" , studentbranchForm : "none", managestudentbranch : "none",eventR : "none", eventRAdd : "none", eventRApp : "none", eventAtt : "none"});
-                break;
-            case "EventView": setDisplay({ event: "none", memberreq: "none", eventView: "", eventForm: "none", memberadd: "none", designation: "none", activity: "none", createDesignationForm: "none", designationChair: "none", editAssigned: "none", editDesignation: "none" , studentbranch : "none" , studentbranchForm : "none", managestudentbranch : "none",eventR : "none", eventRAdd : "none", eventRApp : "none", eventAtt : "none"});
-                break;
-            case "EventForm": setDisplay({ event: "none", memberreq: "none", eventView: "none", eventForm: "", memberadd: "none", designation: "none", activity: "none", createDesignationForm: "none", designationChair: "none", editAssigned: "none", editDesignation: "none" , studentbranch : "none" , studentbranchForm : "none", managestudentbranch : "none",eventR : "none", eventRAdd : "none", eventRApp : "none", eventAtt : "none"});
-                break;
-            case "Add Member": setDisplay({ event: "none", memberreq: "none", eventView: "none", eventForm: "none", memberadd: "", designation: "none", activity: "none", createDesignationForm: "none", designationChair: "none", editAssigned: "none", editDesignation: "none" , studentbranch : "none" , studentbranchForm : "none", managestudentbranch : "none",eventR : "none", eventRAdd : "none", eventRApp : "none", eventAtt : "none"});
-                break;
-            case "Designation": setDisplay({ event: "none", memberreq: "none", eventView: "none", eventForm: "none", memberadd: "none", designation: "", activity: "none", createDesignationForm: "none", designationChair: "none", editAssigned: "none", editDesignation: "none" , studentbranch : "none" , studentbranchForm : "none", managestudentbranch : "none",eventR : "none", eventRAdd : "none", eventRApp : "none", eventAtt : "none"});
-                break;
-            case "Activity": setDisplay({ event: "none", memberreq: "none", eventView: "none", eventForm: "none", memberadd: "none", designation: "none", activity: "", createDesignationForm: "none", designationChair: "none", editAssigned: "none", editDesignation: "none" , studentbranch : "none" , studentbranchForm : "none", managestudentbranch : "none",eventR : "none", eventRAdd : "none", eventRApp : "none", eventAtt : "none"});
-                break;
-            case "CreateDesignationForm": setDisplay({ event: "none", memberreq: "none", eventView: "none", eventForm: "none", memberadd: "none", designation: "none", activity: "none", createDesignationForm: "", designationChair: "none", editAssigned: "none", editDesignation: "none" , studentbranch : "none" , studentbranchForm : "none", managestudentbranch : "none",eventR : "none", eventRAdd : "none", eventRApp : "none", eventAtt : "none"});
-                break;
-            case "DesignationChair": setDisplay({ event: "none", memberreq: "none", eventView: "none", eventForm: "none", memberadd: "none", designation: "none", activity: "none", createDesignationForm: "none", designationChair: "", editAssigned: "none", editDesignation: "none" , studentbranch : "none" , studentbranchForm : "none", managestudentbranch : "none",eventR : "none", eventRAdd : "none", eventRApp : "none", eventAtt : "none"});
-                break;
-            case "EditAssignedMemberForm": setDisplay({ event: "none", memberreq: "none", eventView: "none", eventForm: "none", memberadd: "none", designation: "none", activity: "none", createDesignationForm: "none", designationChair: "none", editAssigned: "", editDesignation: "none" , studentbranch : "none" , studentbranchForm : "none", managestudentbranch : "none",eventR : "none", eventRAdd : "none", eventRApp : "none", eventAtt : "none"});
-                break;
-            case "EditDesignation": setDisplay({ event: "none", memberreq: "none", eventView: "none", eventForm: "none", memberadd: "none", designation: "none", activity: "none", createDesignationForm: "none", designationChair: "none", editAssigned: "none", editDesignation: "" , studentbranch : "none" , studentbranchForm : "none", managestudentbranch : "none",eventR : "none", eventRAdd : "none", eventRApp : "none", eventAtt : "none"});
-                break;
-            case "ManageStudentBranch" : setDisplay({managestudentbranch : "", event:"none", studentbranch : "none" , studentbranchForm : "none" , eventView : "none",eventForm : "none", memberreq : "none", memberadd : "none", designation: "none", activity: "none", createDesignationForm: "none", designationChair: "none", editAssigned: "none", editDesignation: "none",eventR : "none", eventRAdd : "none", eventRApp : "none", eventAtt : "none"});
-                break;
-            case "StudentBranchForm" : setDisplay({studentbranchForm : "", event:"none", studentbranch : "none" , managestudentbranch : "none" ,eventView : "none",eventForm : "none", memberreq : "none", memberadd : "none", designation: "none", activity: "none", createDesignationForm: "none", designationChair: "none", editAssigned: "none", editDesignation: "none",eventR : "none", eventRAdd : "none", eventRApp : "none", eventAtt : "none"});
-                break;
-            case "StudentBranch" : setDisplay({studentbranch : "", event:"none", studentbranchForm : "none" , managestudentbranch : "none" , eventView : "none",eventForm : "none", memberreq : "none", memberadd : "none", designation: "none", activity: "none", createDesignationForm: "none", designationChair: "none", editAssigned: "none", editDesignation: "none",eventR : "none", eventRAdd : "none", eventRApp : "none", eventAtt : "none"});
-                break;
-            case "VReport" : setDisplay({eventR : "", event:"none", eventRAdd : "none", eventRApp : "none", eventAtt : "none",event: "none", memberreq: "none", eventView: "none", eventForm: "none", memberadd: "none", designation: "none", activity: "none", createDesignationForm: "none", designationChair: "none", editAssigned: "none", editDesignation: "none" , studentbranch : "none" , studentbranchForm : "none", managestudentbranch : "none"});
-            break;
-            case "AReport" : setDisplay({eventRAdd : "", eventR : "none", eventRApp : "none", eventAtt : "none",event: "none", memberreq: "none", eventView: "none", eventForm: "none", memberadd: "none", designation: "none", activity: "none", createDesignationForm: "none", designationChair: "none", editAssigned: "none", editDesignation: "none" , studentbranch : "none" , studentbranchForm : "none", managestudentbranch : "none"});
-            break;
-            case "EReport" : setDisplay({eventRApp : "none", eventR : "none", eventRAdd : "", eventAtt : "none",event: "none", memberreq: "none", eventView: "none", eventForm: "none", memberadd: "none", designation: "none", activity: "none", createDesignationForm: "none", designationChair: "none", editAssigned: "none", editDesignation: "none" , studentbranch : "none" , studentbranchForm : "none", managestudentbranch : "none"});
-            break;
-            case "EAttendance" : setDisplay({eventAtt : "", eventR : "none", eventRAdd : "none", eventRApp : "none",event: "none", memberreq: "none", eventView: "none", eventForm: "none", memberadd: "none", designation: "none", activity: "none", createDesignationForm: "none", designationChair: "none", editAssigned: "none", editDesignation: "none" , studentbranch : "none" , studentbranchForm : "none", managestudentbranch : "none"});
-            break;
-            default: setDisplay({ event: "none", memberreq: "none", eventView: "none", eventForm: "none", memberadd: "none", designation: "none", activity: "none", createDesignationForm: "none", designationChair: "none", editAssigned: "none", editDesignation: "none" , studentbranch : "none" , studentbranchForm : "none", managestudentbranch : "none" ,eventR : "none", eventRAdd : "none", eventRApp : "none", eventAtt : "none"});
-                break;
-        }
-
-            
-    }
-      
+  
    return (
     <div className="wrapper">
       <NavBar />
-      <SideBar onClick={showContent} />
       <div className="content-wrapper">
-        <ContentHeader pageName={page}/>
-        <EventTable  display={display.event} onClick={showContent}/>
-        <MemberRequest display={display.memberreq} onClick={showContent}/>
-        <MemberAdd display={display.memberadd} onClick={showContent}/>
-        <DesignationTable display={display.designation} onClick={showContent} />
-        <ActivityTable display={display.activity}/>
-        <CreateDesignationForm display={display.createDesignationForm} />
-        <AssignedDesignationsTable display={display.designationChair} onClick={showContent} />
-        <EditAssignedMemberForm display={display.editAssigned} />
-        <EditDesignation display={display.editDesignation} />
-        <ManageStudentBranch display={display.managestudentbranch}/>
-        <StudentBranchForm display={display.studentbranchForm}/>
-        <StudentBranchTable display={display.studentbranch} onClick={showContent}/>
-        <EventAttendance display={display.eventAtt} onClick={showContent}/>
-        <EventReportForm display={display.eventRAdd} onClick={showContent}/>
-        <EventReportTable display={display.eventR} onClick={showContent}/>
-        
-        <EventView display={display.eventView} onClick={showContent}/>
-        <EventForm display={display.eventForm} onClick={showContent}/>
-        {/* {page === "Event" && <EventTable />} */}
-        {/* {page === "Member" && <MemberTable />} */}
+        <Router>
+            <Switch>
+                    <Route path="/EventTable"> <EventTable page="Event Management"/> </Route>
+                    <Route path="/MemberRequest"> <MemberRequest /> </Route>
+                    <Route path="/EventView/:eventId"> <EventView page="Event Management"/> </Route>
+                    <Route path="/EventForm"> <EventForm page="Event Management"/> </Route>
+                    <Route path="/AffiliationTable"> <AffiliationTable /> </Route>
+                    <Route path="/StudentForm"> <StudentForm /> </Route>
+                    <Route path="/ManageAffiliation"> <ManageAffiliation /> </Route>
+            </Switch>
+            <SideBar/> 
+        </Router>
       </div>
       <Footer />
     </div>
