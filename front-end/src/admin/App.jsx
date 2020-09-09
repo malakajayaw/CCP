d724fdae929eda8b68a84b096262f38df3d99bc0
Thimithi Weeratunga <wthimithi@gmail.com>
2020-09-09 18:03:07 +0000

    App.jsx edited online with Bitbucket

--- front-end/src/admin/App.jsx
+++ front-end/src/admin/App.jsx
@@ -31,13 +31,26 @@ function App() {
       <div className="content-wrapper">
         <Router>
             <Switch>
-                    <Route path="/EventTable"> <EventTable page="Event Management"/> </Route>
-                    <Route path="/MemberRequest"> <MemberRequest /> </Route>
-                    <Route path="/EventView/:eventId"> <EventView page="Event Management"/> </Route>
-                    <Route path="/EventForm"> <EventForm page="Event Management"/> </Route>
-                    <Route path="/AffiliationTable"> <AffiliationTable /> </Route>
-                    <Route path="/StudentForm"> <StudentForm /> </Route>
+                    <Route path="/Admin/EventTable"> <EventTable page="Event Management"/> </Route>
+                    <Route path="/Admin/MemberRequest"> <MemberRequest /> </Route>
+                    <Route path="/Admin/EventView/:eventId"> <EventView page="Event Management"/> </Route>
+                    <Route path="/Admin/EventForm"> <EventForm page="Event Management"/> </Route>
+                    <Route path="/Admin/MemberList"> <MemberList /> </Route>
+                    <Route path="/Admin/MemberAdd"> <MemberAdd /> </Route>
+                    <Route path="/Admin/MemberEdit/:id"> <MemberEdit /> </Route>
+                    <Route path="/DesignationAdmin"> <DesignationTable /> </Route>
+                    <Route path="/DesignationChair"> <AssignedDesignationsTable /> </Route>
+                    <Route path="/ActivityLog"> <ActivityTable /> </Route>
+                    <Route path="/AddDesignation"> <CreateDesignationForm /> </Route>
+                    <Route path="/EditDesignation/:desId"> <EditDesignation /> </Route>
+                    <Route path="/AssignMember"> <EditAssignedMemberForm /> </Route>
+                    <Route path="/Admin/EventReportTable"> <EventReportTable/> </Route>
+                    <Route path="/Admin/EventReportForm"> <EventReportForm/> </Route>
+                    <Route path="/Admin/EventAttendance"> <EventAttendance/> </Route>
+                    <Route path="/Admin/EventReportView/:id"> <EventReportView/> </Route>
                     <Route path="/ManageAffiliation"> <ManageAffiliation /> </Route>
+                    <Route path="/StudentForm"> <StudentForm /> </Route>
+                    <Route path="/AffiliationTable"> <AffiliationTable /> </Route>
             </Switch>
             <SideBar/> 
         </Router>
