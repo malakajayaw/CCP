33f267616bdd04889b200f7c57f07e2ac4029800
Thimithi Weeratunga <wthimithi@gmail.com>
2020-09-09 18:06:54 +0000

    App.jsx edited online with Bitbucket

--- front-end/src/admin/App.jsx
+++ front-end/src/admin/App.jsx
@@ -9,26 +9,36 @@ import EventView from './components/Event-management/EventView';
 import EventForm from './components/Event-management/EventForm';
 import MemberRequest from './components/Member-management/MemberRequest';
 import MemberAdd from './components/Member-management/MemberAdd';
+import MemberList from './components/Member-management/MemberList';
 import DesignationTable from './components/Designation-management/DesignationTable';
-import CreateDesignationForm from './components/Designation-management/CreateDesignationForm';
 import AssignedDesignationsTable from './components/Designation-management/AssignedDesignationsTable';
+import CreateDesignationForm from './components/Designation-management/CreateDesignationForm';
 import EditAssignedMemberForm from './components/Designation-management/EditAssignedMemberForm';
 import EditDesignation from './components/Designation-management/EditDesignation';
 import ActivityTable from './components/Activity-log/ActivityTable';
-import EventReportTable from './components/EventReport-management/EventReportTable';
-import EventReportForm from './components/EventReport-management/EventReportForm';
-import EventAttendance from './components/EventAttendance-management/EventAttendance';
-import AffiliationTable from './components/StudentBranch-management/AffiliationTable';
-import StudentForm from './components/StudentBranch-management/StudentForm'
+import AdminLogin from './components/Login/AdminLogin';
+import MemberEdit from './components/Member-management/MemberEdit';
 import ManageAffiliation from './components/StudentBranch-management/ManageAffiliation'
-import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import StudentForm from './components/StudentBranch-management/StudentForm'
+import AffiliationTable from './components/StudentBranch-management/AffiliationTable';
 
+import EventReportTable from './components/EventReport-management/EventReportTable'
+import EventReportForm from './components/EventReport-management/EventReportForm'
+import EventAttendance from './components/EventAttendance-management/EventAttendance'
+import EventReportView from './components/EventReport-management/EventReportView'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+toast.configure() 
 function App() {
-  
+    const [page, setPage] = useState("Event");
+              
    return (
     <div className="wrapper">
       <NavBar />
+      
       <div className="content-wrapper">
+  
         <Router>
             <Switch>
                     <Route path="/Admin/EventTable"> <EventTable page="Event Management"/> </Route>
@@ -54,6 +64,7 @@ function App() {
             </Switch>
             <SideBar/> 
         </Router>
+                    
       </div>
       <Footer />
     </div>
