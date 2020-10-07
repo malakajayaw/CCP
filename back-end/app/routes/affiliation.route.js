d3789fd8b087945561afe2cb646c48c4e482e571
Thimithi Weerathunga <20201964@student.curtin.edu.au>
2020-10-07 21:40:40 +0530

    Final Fixes

---
+++ back-end/app/routes/affiliation.route.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const AffiliationController = require('../controllers/affiliation.controller');
+
+// add affiliation
+//router.post('/addAffiliation', AffiliationController.addAffiliation);
+
+//get all affiliation
+//router.get('/getAffiliation', AffiliationController.get_all_affiliation);
+
+//get edit affiliation
+//router.get('/editAffiliation', AffiliationController.editAffiliation);
+
+module.exports = router;
