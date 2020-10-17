cd6cadf84c09e06355e7f14608bcc8d71d2f20b9
Malaka Jayawardhana <malaka.jayaw@gmail.com>
2020-10-17 21:38:27 +0530

    unwanted file removal carried over

--- back-end/server.js
+++ back-end/server.js
@@ -16,7 +16,6 @@ const eventAttendanceRoutes = require("./app/routes/event.attendance.route");
 const eventReport = require("./app/routes/event.report.route");
 const activity = require("./app/routes/activity.route");
 const designations = require("./app/routes/designations.route");
-const assignments = require("./app/routes/designationsAss.route");
 const affiliation = require("./app/routes/affiliation.route");
 const pastdes = require("./app/routes/pastdes.route");
 
@@ -47,7 +46,6 @@ app.use("/eventReport", eventReport);
 app.use("/eventattendance", eventAttendanceRoutes);
 app.use("/activity", activity);
 app.use("/designations", designations);
-app.use("/assignments", assignments);
 app.use("/affiliation", affiliation)
 app.use("/pastdes", pastdes)
 
