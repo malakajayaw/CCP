9c01d81a28e6f7ba6bb671a53f67c2f2c69e937e
Prabhasha Amarathunga <prabhasha.amarathunga@gmail.com>
2020-10-19 21:46:00 +0530

    Minor Changes

--- front-end/src/admin/App.jsx
+++ front-end/src/admin/App.jsx
@@ -25,7 +25,6 @@ import EditDesignation from "./components/Designation-management/EditDesignation
 import EditPastDes from "./components/Designation-management/EditPastDes";
 import EditPastDesForAff from "./components/Designation-management/EditPastDesForAff";
 import ActivityTable from "./components/Activity-log/ActivityTable";
-import AdminLogin from "./components/Login/AdminLogin";
 import MemberEdit from "./components/Member-management/MemberEdit";
 import EventReportTable from "./components/EventReport-management/EventReportTable";
 import EventReportForm from "./components/EventReport-management/EventReportForm";
@@ -39,11 +38,8 @@ import AffiliationTable from "./components/StudentBranch-management/AffiliationT
 import AdminAdd from "./components/Member-management/AdminAdd";
 import AdminList from "./components/Member-management/AdminList";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { store, persistor } from "./components/Redux/Store/Store";
 toast.configure();
 
 function App() {
@@ -202,8 +198,6 @@ function App() {
           </div>
           <Footer />
         </div>
-      </PersistGate>
-    </Provider>
   );
 }
 
